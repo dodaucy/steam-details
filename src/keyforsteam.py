1283e188b93e5606c27e7180f7170d4dabf60dfa
XGamerGuide <xgamerguide.offiziell@gmail.com>
2024-07-23 17:07:05 +0200

    Fix cheapest offer price under historical low price bug

--- src/keyforsteam.py
+++ src/keyforsteam.py
@@ -62,16 +62,24 @@ async def get_key_and_gift_sellers_data(name: str) -> Union[dict, None]:
     r.raise_for_status()
     price_history_data = r.json()
 
+    cheapest_offer_price = round(cheapest_offer["price"]["eur"]["priceCard"], 2)
+    cheapest_offer_seller = offers_data["merchants"][str(cheapest_offer["merchant"])]["name"]
+    historical_low_price = price_string_to_float(price_history_data["lower_keyshops_price"]["price"])
+    historical_low_seller = price_history_data["merchants"][price_history_data["lower_keyshops_price"]["merchant_id"]]["name"]
+    if cheapest_offer_price < historical_low_price:
+        historical_low_price = cheapest_offer_price
+        historical_low_seller = cheapest_offer_seller
+
     return {
         "cheapest_offer": {
-            "price": round(cheapest_offer["price"]["eur"]["priceCard"], 2),
+            "price": cheapest_offer_price,
             "form": offers_data["regions"][cheapest_offer["region"]]["name"],
-            "seller": offers_data["merchants"][str(cheapest_offer["merchant"])]["name"],
+            "seller": cheapest_offer_seller,
             "edition": offers_data["editions"][cheapest_offer["edition"]]["name"]
         },
         "historical_low": {
-            "price": price_string_to_float(price_history_data["lower_keyshops_price"]["price"]),
-            "seller": price_history_data["merchants"][price_history_data["lower_keyshops_price"]["merchant_id"]]["name"]
+            "price": historical_low_price,
+            "seller": historical_low_seller
         },
         "external_url": f"https://www.keyforsteam.de/{'-'.join(name.lower().split(' '))}-key-kaufen-preisvergleich/"
     }
