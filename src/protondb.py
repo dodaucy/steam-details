39619d0ca9402d4b4f36447ff377718f296d749e
XGamerGuide <xgamerguide.offiziell@gmail.com>
2024-07-24 03:26:41 +0200

    Add missing response print

--- src/protondb.py
+++ src/protondb.py
@@ -6,6 +6,7 @@ from utils import http_client
 async def get_linux_support(appid: str) -> Union[dict, None]:
     print(f"Getting linux support state for {appid}")
     r = await http_client.get(f"https://www.protondb.com/api/v1/reports/summaries/{appid}.json")
+    print(f"Response: {r.text}")
     if r.status_code == 404:
         return
     r.raise_for_status()
