65c0bb98fa86978c89fd17cbafeb25c91de28892
XGamerGuide <xgamerguide.offiziell@gmail.com>
2024-07-25 20:39:50 +0200

    Fix howlongtobeat bug due to weird api changes

--- src/how_long_to_beat.py
+++ src/how_long_to_beat.py
@@ -7,42 +7,45 @@ from utils import http_client
 
 async def get_game_length(steam: SteamDetails) -> Union[dict, None]:
     logging.info(f"Getting how long to beat for {repr(steam.name)} ({steam.appid})")
-    r = await http_client.post(
-        "https://howlongtobeat.com/api/find",
-        headers={
-            "Origin": "https://howlongtobeat.com",
-            "Priority": "u=4",
-            "Referer": "https://howlongtobeat.com/",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-GPC": "1",
-        },
-        json={
-            "searchType": "games",
-            "searchTerms": [steam.name],
-            "searchPage": 1,
-            "size": 10,
-            "searchOptions": {
-                "games": {
-                    "userId": 0,
-                    "platform": "PC",
-                    "sortCategory": "name",
-                    "rangeCategory": "main",
-                    "rangeTime": {"min": None, "max": None},
-                    "gameplay": {"perspective": "", "flow": "", "genre": ""},
-                    "rangeYear": {"min": "", "max": ""},
-                    "modifier": "",
-                },
-                "users": {"sortCategory": "postcount"},
-                "lists": {"sortCategory": "follows"},
-                "filter": "",
-                "sort": 0,
-                "randomizer": 0,
+    for url in ("https://howlongtobeat.com/api/search", "https://howlongtobeat.com/api/find"):
+        r = await http_client.post(
+            url,
+            headers={
+                "Origin": "https://howlongtobeat.com",
+                "Priority": "u=4",
+                "Referer": "https://howlongtobeat.com/",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-GPC": "1",
             },
-            "useCache": True,
-        }
-    )
+            json={
+                "searchType": "games",
+                "searchTerms": [steam.name],
+                "searchPage": 1,
+                "size": 10,
+                "searchOptions": {
+                    "games": {
+                        "userId": 0,
+                        "platform": "PC",
+                        "sortCategory": "name",
+                        "rangeCategory": "main",
+                        "rangeTime": {"min": None, "max": None},
+                        "gameplay": {"perspective": "", "flow": "", "genre": ""},
+                        "rangeYear": {"min": "", "max": ""},
+                        "modifier": "",
+                    },
+                    "users": {"sortCategory": "postcount"},
+                    "lists": {"sortCategory": "follows"},
+                    "filter": "",
+                    "sort": 0,
+                    "randomizer": 0,
+                },
+                "useCache": True,
+            }
+        )
+        if r.status_code != 404:
+            break
     logging.info(f"Response (100 chars): {repr(r.text[:100])}")
     logging.debug(f"Response: (all): {r.text}")
     r.raise_for_status()
