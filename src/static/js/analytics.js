f429fa094cf0f73af28d0bc86823905ff9ee5dca
dodaucy <dodaucy@gmail.com>
2024-10-01 22:31:10 +0200

    Update analytics stats front end

--- src/static/js/analytics.js
+++ src/static/js/analytics.js
@@ -22,64 +22,90 @@ async function analyze() {
         elements.push(errorDiv);
     }
 
-    if (elements.length == 0) {
+    if (elements.length == 0) {  // No error
         // Display service stats
         const serviceStats = document.createElement("div");
         serviceStats.id = "service-stats";
-        Object.keys(data.services).forEach((service) => {
+        serviceStats.className = "center-text";
+
+        for (const service of data.services) {
             const serviceElement = document.createElement("div");
 
             // Title
             const serviceTitle = document.createElement("div");
-            serviceTitle.innerText = service;
+            serviceTitle.innerText = service.name;
             serviceTitle.className = "title";
             serviceElement.appendChild(serviceTitle);
 
-            // Stats
-            const serviceStatsElement = document.createElement("div");
-            Object.keys(data.services[service]).forEach((key) => {
-                const serviceStat = document.createElement("div");
-                let stat_name = key;
-                let end = "";
-                if (key == "load_time") {  // Load time
-                    stat_name = "Load Time";
-                    end = "s";
-                    if (data.services[service][key] > 10) {  // Very high load time
-                        serviceStat.className = "red-text";
-                    } else if (data.services[service][key] > 5) {  // High load time
-                        serviceStat.className = "orange-text";
-                    } else if (data.services[service][key] > 3) {  // Medium load time
-                        serviceStat.className = "yellow-text";
-                    } else {  // Low load time
-                        serviceStat.className = "green-text";
-                    }
-                } else if (key == "timeout_count") {  // Timeout count
-                    stat_name = "Timeout Count";
-                    if (data.services[service][key] > 3) {  // Many timeouts
-                        serviceStat.className = "red-text";
-                    } else if (data.services[service][key] > 0) {  // Timeouts
-                        serviceStat.className = "orange-text";
-                    } else {  // No timeouts
-                        serviceStat.className = "green-text";
-                    }
-                } else if (key == "error_count") {  // Error count
-                    stat_name = "Error Count";
-                    if (data.services[service][key] > 3) {  // Many errors
-                        serviceStat.className = "red-text";
-                    } else if (data.services[service][key] > 0) {  // Errors
-                        serviceStat.className = "orange-text";
-                    } else {  // No errors
-                        serviceStat.className = "green-text";
-                    }
-                }
-                serviceStat.innerText = `${stat_name}: ${data.services[service][key]}${end}`;
-                serviceStatsElement.appendChild(serviceStat);
+            // Load time
+            const serviceLoadTime = document.createElement("div");
 
-                serviceElement.appendChild(serviceStatsElement);
+            const serviceLoadTimeTitle = document.createElement("div");
+            serviceLoadTimeTitle.innerText = "Load Time";
+            serviceLoadTime.appendChild(serviceLoadTimeTitle);
 
-            })
+            const serviceLoadTimeValue = document.createElement("div");
+            serviceLoadTimeValue.innerText = service.load_time + "s";
+            serviceLoadTime.appendChild(serviceLoadTimeValue);
+
+            if (service.load_time > 10) {  // Very high load time
+                serviceLoadTimeValue.className = "red-text";
+            } else if (service.load_time > 5) {  // High load time
+                serviceLoadTimeValue.className = "orange-text";
+            } else if (service.load_time > 3) {  // Medium load time
+                serviceLoadTimeValue.className = "yellow-text";
+            } else {  // Low load time
+                serviceLoadTimeValue.className = "green-text";
+            }
+
+            serviceElement.appendChild(serviceLoadTime);
+
+            // Timeout count
+            const serviceTimeoutCount = document.createElement("div");
+
+            const serviceTimeoutCountTitle = document.createElement("div");
+            serviceTimeoutCountTitle.innerText = "Timeout Count";
+            serviceTimeoutCount.appendChild(serviceTimeoutCountTitle);
+
+            const serviceTimeoutCountValue = document.createElement("div");
+            serviceTimeoutCountValue.innerText = service.timeout_count;
+            serviceTimeoutCount.appendChild(serviceTimeoutCountValue);
+
+            if (service.timeout_count >= 3) {  // Many timeouts
+                serviceTimeoutCountValue.className = "red-text";
+            } else if (service.timeout_count > 0) {  // Timeouts
+                serviceTimeoutCountValue.className = "orange-text";
+            } else {  // No timeouts
+                serviceTimeoutCountValue.className = "green-text";
+            }
+
+            serviceElement.appendChild(serviceTimeoutCount);
+
+            // Error count
+            const serviceErrorCount = document.createElement("div");
+
+            const serviceErrorCountTitle = document.createElement("div");
+            serviceErrorCountTitle.innerText = "Error Count";
+            serviceErrorCount.appendChild(serviceErrorCountTitle);
+
+            const serviceErrorCountValue = document.createElement("div");
+            serviceErrorCountValue.innerText = service.error_count;
+            serviceErrorCount.appendChild(serviceErrorCountValue);
+
+            if (service.error_count >= 3) {  // Many errors
+                serviceErrorCountValue.className = "red-text";
+            } else if (service.error_count > 0) {  // Errors
+                serviceErrorCountValue.className = "orange-text";
+            } else {  // No errors
+                serviceErrorCountValue.className = "green-text";
+            }
+
+            serviceElement.appendChild(serviceErrorCount);
+
+            // Add to list
             serviceStats.appendChild(serviceElement);
-        });
+        }
+
         elements.push(serviceStats);
 
         // Display speed box plot
