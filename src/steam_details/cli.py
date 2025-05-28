646a9d03293294a4f19053249c4882343aac9d61
dodaucy <dodaucy@gmail.com>
2025-05-28 11:26:41 +0200

    Add app info to args parser

--- src/steam_details/cli.py
+++ src/steam_details/cli.py
@@ -1,7 +1,11 @@
 from argparse import ArgumentParser
 
+parser = ArgumentParser(
+    prog="steam_details",
+    description="Displays some details for a steam app or a whole wishlist.",
+    epilog="If you find bugs or have suggestions, please open an issue at https://github.com/dodaucy/steam-details/issues/new/choose",
+)
 
-parser = ArgumentParser()
 parser.add_argument(
     "-v", "--version", action="store_true", help="Show the version and exit."
 )
